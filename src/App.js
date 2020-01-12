8ce95e42c20ba7f64ce5e08780b8409eeb6fd7a3
Isabelly <isabelly.paganini@gmail.com>
2020-01-12 18:29:56 -0300

    Curso React Platzi - Praticando

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import NavBar from './NavBar';
+import Home from './Home';
+import Brasileirao from './Brasileirao';
+import LiveMatch from './LiveMatch';
+
+
+const App = () => (
+    <BrowserRouter>
+    <>
+        <NavBar />
+        <main>
+            <Route exact path="/" component={Home} />
+            <Route path="/brasileirao" component={Brasileirao} />
+            <Route path="/ao-vivo" component={LiveMatch} />
+        </main>
+    </>
+    </BrowserRouter>
+);
 
 export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { Link } from 'react-router-dom';
+
+import './NavBar.css';
+
+const links = [{
+    to: '/',
+    text: 'Home'
+}, {
+    to: '/brasileirao',
+    text: 'Brasileirão 2019'
+}, {
+    to: '/ao-vivo',
+    text: 'Espanha x Suíça Ao Vivo'
+}]
+
+const NavBar = () => {
+    const isBigEnough = useMediaQuery({ query: "(min-width: 575px)" });
+
+    const [open, setOpen] = React.useState();
+
+    const appLinks = links.map(l => (
+        <Link key={l.to} to={l.to} className="nav-link">
+            {l.text}
+        </Link>
+    ))
+
+    return (
+        <nav className="nav-bar">
+            {isBigEnough ? (
+                <div className="nav-big">
+                    {appLinks}
+                </div>
+            ) : (
+                <div className="nav-small">
+                    <button
+                        className="nav-small__button"
+                        onClick={() => setOpen(o => !o)}
+                        style={{
+                            borderBottom: `1px solid ${open ? "#fff" : "transparent"}`
+                    }}>
+                        MENU
+                    </button>
+                    <div
+                        className="nav-small__links"
+                        style={{ display: open ? "block" : "none" }}>
+                        {appLinks}
+                    </div>
+                </div>
+            )}
+        </nav>
+    );
+}
+
+export default NavBar;
